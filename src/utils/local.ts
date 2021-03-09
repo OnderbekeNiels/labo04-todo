a925ac48a3564c4003f2e3057c4c58ff08701e04
OnderbekeNiels <niels.onderbeke@student.howest.be>
2021-03-09 11:41:41 +0100

    afronden ts

---
+++ src/utils/local.ts
@@ -0,0 +1,24 @@
+// todo: how to keep track of todos in localstorage?
+
+const localTodos = (function () {
+    // Deze iife || self invocing function maakt een 'state' aan en is dus een closure.
+
+    const InsertToDo = (toDo) => {
+        
+    };
+
+    const DeleteToDo = (toDo) => {
+        
+    };
+
+    const UpdateToDo = (toDo) => {
+
+    };
+
+    const ReadToDos = () => {
+
+    };
+
+
+    return { InsertToDo, DeleteToDo, UpdateToDo, ReadToDos };
+})();
